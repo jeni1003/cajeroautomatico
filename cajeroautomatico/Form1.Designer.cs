93667aba316d82cf2135f8b94233ae0f87c19b72
WilsonGutierrez <60446487+WilsonGutierrez@users.noreply.github.com>
2020-02-10 11:10:57 -0600

    Actualizacionn

--- cajeroautomatico/Form1.Designer.cs
+++ cajeroautomatico/Form1.Designer.cs
@@ -28,12 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDinero = new System.Windows.Forms.Button();
+            this.txtDinero = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtDinero1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal2 = new System.Windows.Forms.Label();
+            this.txtDinero2 = new System.Windows.Forms.TextBox();
+            this.btnDinero1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFibo = new System.Windows.Forms.Label();
+            this.lblFiboResp = new System.Windows.Forms.Label();
+            this.txtFibocci = new System.Windows.Forms.TextBox();
+            this.btnFibocci = new System.Windows.Forms.Button();
+            this.btnPrm = new System.Windows.Forms.Button();
+            this.lblPrimoResp = new System.Windows.Forms.Label();
+            this.txtPrm = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDinero
+            // 
+            this.btnDinero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDinero.Location = new System.Drawing.Point(33, 56);
+            this.btnDinero.Name = "btnDinero";
+            this.btnDinero.Size = new System.Drawing.Size(71, 23);
+            this.btnDinero.TabIndex = 11;
+            this.btnDinero.Text = "Retirar";
+            this.btnDinero.UseVisualStyleBackColor = true;
+            this.btnDinero.Click += new System.EventHandler(this.btnDinero_Click);
+            // 
+            // txtDinero
+            // 
+            this.txtDinero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinero.Location = new System.Drawing.Point(25, 28);
+            this.txtDinero.Name = "txtDinero";
+            this.txtDinero.Size = new System.Drawing.Size(100, 22);
+            this.txtDinero.TabIndex = 10;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(12, 91);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(52, 16);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total: ?";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(42, 9);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(62, 16);
+            this.lblCantidad.TabIndex = 8;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // txtDinero1
+            // 
+            this.txtDinero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinero1.Location = new System.Drawing.Point(170, 26);
+            this.txtDinero1.Name = "txtDinero1";
+            this.txtDinero1.Size = new System.Drawing.Size(100, 21);
+            this.txtDinero1.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Cantidad";
+            // 
+            // lblTotal2
+            // 
+            this.lblTotal2.AutoSize = true;
+            this.lblTotal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal2.Location = new System.Drawing.Point(248, 53);
+            this.lblTotal2.Name = "lblTotal2";
+            this.lblTotal2.Size = new System.Drawing.Size(47, 15);
+            this.lblTotal2.TabIndex = 27;
+            this.lblTotal2.Text = "Total: ?";
+            // 
+            // txtDinero2
+            // 
+            this.txtDinero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinero2.Location = new System.Drawing.Point(296, 26);
+            this.txtDinero2.Name = "txtDinero2";
+            this.txtDinero2.Size = new System.Drawing.Size(100, 21);
+            this.txtDinero2.TabIndex = 26;
+            // 
+            // btnDinero1
+            // 
+            this.btnDinero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDinero1.Location = new System.Drawing.Point(170, 53);
+            this.btnDinero1.Name = "btnDinero1";
+            this.btnDinero1.Size = new System.Drawing.Size(56, 27);
+            this.btnDinero1.TabIndex = 25;
+            this.btnDinero1.Text = "Pagar";
+            this.btnDinero1.UseVisualStyleBackColor = true;
+            this.btnDinero1.Click += new System.EventHandler(this.btnDinero1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Cantidad a pagar";
+            // 
+            // lblFibo
+            // 
+            this.lblFibo.AutoSize = true;
+            this.lblFibo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFibo.Location = new System.Drawing.Point(456, 36);
+            this.lblFibo.Name = "lblFibo";
+            this.lblFibo.Size = new System.Drawing.Size(60, 15);
+            this.lblFibo.TabIndex = 33;
+            this.lblFibo.Text = "Fibonacci";
+            // 
+            // lblFiboResp
+            // 
+            this.lblFiboResp.AutoSize = true;
+            this.lblFiboResp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiboResp.Location = new System.Drawing.Point(699, 39);
+            this.lblFiboResp.Name = "lblFiboResp";
+            this.lblFiboResp.Size = new System.Drawing.Size(76, 15);
+            this.lblFiboResp.TabIndex = 32;
+            this.lblFiboResp.Text = "Resultado: ?";
+            // 
+            // txtFibocci
+            // 
+            this.txtFibocci.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFibocci.Location = new System.Drawing.Point(522, 36);
+            this.txtFibocci.Name = "txtFibocci";
+            this.txtFibocci.Size = new System.Drawing.Size(72, 21);
+            this.txtFibocci.TabIndex = 31;
+            // 
+            // btnFibocci
+            // 
+            this.btnFibocci.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFibocci.Location = new System.Drawing.Point(627, 36);
+            this.btnFibocci.Name = "btnFibocci";
+            this.btnFibocci.Size = new System.Drawing.Size(66, 26);
+            this.btnFibocci.TabIndex = 30;
+            this.btnFibocci.Text = "Realizar";
+            this.btnFibocci.UseVisualStyleBackColor = true;
+            this.btnFibocci.Click += new System.EventHandler(this.btnFibocci_Click);
+            // 
+            // btnPrm
+            // 
+            this.btnPrm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrm.Location = new System.Drawing.Point(627, 7);
+            this.btnPrm.Name = "btnPrm";
+            this.btnPrm.Size = new System.Drawing.Size(71, 25);
+            this.btnPrm.TabIndex = 37;
+            this.btnPrm.Text = "Realizar";
+            this.btnPrm.UseVisualStyleBackColor = true;
+            this.btnPrm.Click += new System.EventHandler(this.btnPrm_Click);
+            // 
+            // lblPrimoResp
+            // 
+            this.lblPrimoResp.AutoSize = true;
+            this.lblPrimoResp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimoResp.Location = new System.Drawing.Point(704, 13);
+            this.lblPrimoResp.Name = "lblPrimoResp";
+            this.lblPrimoResp.Size = new System.Drawing.Size(79, 15);
+            this.lblPrimoResp.TabIndex = 36;
+            this.lblPrimoResp.Text = "Respuesta: ?";
+            // 
+            // txtPrm
+            // 
+            this.txtPrm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrm.Location = new System.Drawing.Point(521, 7);
+            this.txtPrm.Name = "txtPrm";
+            this.txtPrm.Size = new System.Drawing.Size(100, 21);
+            this.txtPrm.TabIndex = 35;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(427, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 15);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Numero Primo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(910, 334);
+            this.Controls.Add(this.btnPrm);
+            this.Controls.Add(this.lblPrimoResp);
+            this.Controls.Add(this.txtPrm);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFibo);
+            this.Controls.Add(this.lblFiboResp);
+            this.Controls.Add(this.txtFibocci);
+            this.Controls.Add(this.btnFibocci);
+            this.Controls.Add(this.txtDinero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTotal2);
+            this.Controls.Add(this.txtDinero2);
+            this.Controls.Add(this.btnDinero1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDinero);
+            this.Controls.Add(this.txtDinero);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCantidad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cajero";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDinero;
+        private System.Windows.Forms.TextBox txtDinero;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtDinero1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotal2;
+        private System.Windows.Forms.TextBox txtDinero2;
+        private System.Windows.Forms.Button btnDinero1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFibo;
+        private System.Windows.Forms.Label lblFiboResp;
+        private System.Windows.Forms.TextBox txtFibocci;
+        private System.Windows.Forms.Button btnFibocci;
+        private System.Windows.Forms.Button btnPrm;
+        private System.Windows.Forms.Label lblPrimoResp;
+        private System.Windows.Forms.TextBox txtPrm;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- cajeroautomatico/Form1.cs
+++ cajeroautomatico/Form1.cs
@@ -16,5 +16,86 @@ namespace cajeroautomatico
         {
             InitializeComponent();
         }
+
+        private void btnDinero_Click(object sender, EventArgs e)
+        {
+            double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+            double cantidad = double.Parse(txtDinero.Text);
+            sbyte n = 0;
+            string respuesta = "Total: \n";
+            foreach (double denominacion in denominaciones)
+            {
+                while (denominacion <= Math.Round(cantidad, 2))
+                {
+                    n++;
+                    cantidad -= denominacion;
+                }
+                if (n > 0)
+                {
+                    respuesta += n + (denominacion > 1 ? " Billetes de " : " Monedas de ") + denominacion + "\n";
+                }
+                n = 0;
+            }
+            lblTotal.Text = respuesta;
+            txtDinero.Text = "";
+        }
+
+        private void btnDinero1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtDinero1.Text), b = double.Parse(txtDinero2.Text), total;
+            double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+            sbyte n = 0;
+            string respuesta = "Total: \n";
+            foreach (double denominacion in denominaciones)
+            {
+                total = a - b;
+                while (denominacion <= Math.Round(a, 2))
+                {
+                    a++;
+                    //
+                    a -= denominacion;
+                }
+                if (n > 0)
+                {
+                    respuesta += total + "\n" + a + (denominacion > 1 ? " Billetes de " : " Monedas de ") + denominacion;
+                    n = 0;
+                    lblTotal2.Text = respuesta;
+                }
+            }
+        }
+
+        private void btnFibocci_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(txtFibocci.Text), a = 0, b = 1, au, i;
+            string respuesta = "";
+            for (i = 0; i < n; i++)
+            {
+                au = a;
+                a = b;
+                b = au + a;
+                respuesta += "la suma de " + a + " es " + b + "\n";
+                lblFiboResp.Visible = true;
+                lblFiboResp.Text = "" + respuesta;
+                txtFibocci.Text = "";
+            }
+        }
+
+        private void btnPrm_Click(object sender, EventArgs e)
+        {
+            int a = 0, i, n;
+            n = int.Parse(txtPrm.Text);
+            a = 0;
+            for (i = 1; i < (n + 1); i++)
+
+                if (n % i == 0)
+                    a++;
+            lblPrimoResp.Text = ((a != 2) ? n + " No es primo " : n + "  Es primo" + "\n");
+            txtPrm.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
